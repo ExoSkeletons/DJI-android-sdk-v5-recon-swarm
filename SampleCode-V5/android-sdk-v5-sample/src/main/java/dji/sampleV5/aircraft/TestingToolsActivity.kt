da5cfbed4ea03b943ceb7c84eff5438c3e6c050f
ExoSkeleton <calazulazu@gmail.com>
2026-06-09 11:18:07 +0300

    init/release sfx

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/TestingToolsActivity.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/TestingToolsActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.navigation.Navigation
-import dji.sampleV5.aircraft.databinding.ActivityMainBinding
+import com.kcg.dr.SFXManager
 import dji.sampleV5.aircraft.databinding.ActivityTestingToolsBinding
 import dji.sampleV5.aircraft.models.MSDKCommonOperateVm
 import dji.sampleV5.aircraft.util.DJIToastUtil
@@ -31,6 +31,8 @@ abstract class TestingToolsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Init SFX
+        SFXManager.init(this)
         binding = ActivityTestingToolsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -96,6 +98,7 @@ abstract class TestingToolsActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         DJIToastUtil.dJIToastLD = null
+        SFXManager.release()
     }
 
     open fun loadTitleView() {
