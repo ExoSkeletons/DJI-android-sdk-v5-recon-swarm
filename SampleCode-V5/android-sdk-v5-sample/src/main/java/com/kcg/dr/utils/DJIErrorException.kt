2615e2ce2e2e3cec53858169a4e8d609a251d6d5
ExoSkeleton <calazulazu@gmail.com>
2026-07-28 13:59:49 +0300

    detailed dji error message and optional trace

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/com/kcg/dr/utils/DJIErrorException.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/com/kcg/dr/utils/DJIErrorException.kt
@@ -3,4 +3,10 @@ package com.kcg.dr.utils
 import dji.v5.common.error.IDJIError
 import dji.v5.lib.codec.util.DJIRuntimeException
 
-class DJIErrorException(val error: IDJIError) : DJIRuntimeException(error.description())
+class DJIErrorException(val error: IDJIError, throwable: Throwable? = null) :
+    DJIRuntimeException(
+        "${error.errorType()}: " +
+                "${error.errorCode()},${error.innerCode()} ${error.description() ?: ""}" +
+                (error.hint()?.let { "\n$it" } ?: ""),
+        throwable
+    )
