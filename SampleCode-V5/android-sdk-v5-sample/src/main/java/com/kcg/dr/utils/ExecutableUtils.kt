bc7805a5bf684527cfb537fc33661a3a5c246349
ExoSkeleton <calazulazu@gmail.com>
2026-07-30 12:49:42 +0300

    robust lib check

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/com/kcg/dr/utils/ExecutableUtils.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/com/kcg/dr/utils/ExecutableUtils.kt
@@ -10,15 +10,28 @@ import java.io.IOException
 object ExecutableUtils {
     fun Context.getExecutableFromLibs(fileName: String): File {
         val libDir = File(this.applicationInfo.nativeLibraryDir)
-        val libFile = File(libDir, "lib$fileName.so")
-        if (!libFile.exists())
-            throw IOException("Executable lib file $fileName is missing from native library directory ${libDir.absolutePath}.\nDid you forget to put it in jniLibs?")
-        return libFile
+        val candidates = listOf(
+            File(libDir, "lib$fileName.so"),
+            File(libDir, fileName),
+            File(libDir, "lib$fileName"),
+            File(libDir, "$fileName.so"),
+        )
+        for (file in candidates)
+            if (file.exists())
+                return file
+        throw IOException(
+            "Executable lib file $fileName is missing from native library directory ${libDir.absolutePath}.\n" +
+                    "Checked: ${candidates.joinToString(", ") { it.absolutePath }}\n" +
+                    "Did you forget to put it in jniLibs?"
+        )
     }
 
-    @Deprecated("W^X violation on Android 10+", ReplaceWith("getExecutibleFromLibs(context, fileName)"))
+    @Deprecated(
+        "W^X violation on Android 10+",
+        ReplaceWith("getExecutibleFromLibs(context, fileName)")
+    )
     fun Context.getExecutableFromAssets(fileName: String): File {
-        val file = this.getAssetOrExtract( fileName, false)
+        val file = this.getAssetOrExtract(fileName, false)
         Log.d("Asset", "making $fileName executable...")
         // Note: chmod exec perm set fails on Android 10+
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
