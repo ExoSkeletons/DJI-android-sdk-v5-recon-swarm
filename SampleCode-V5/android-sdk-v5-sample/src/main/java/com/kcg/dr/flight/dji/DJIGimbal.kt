fb83d13da868bdbf00a297824bc4f313d5d43aab
ExoSkeleton <calazulazu@gmail.com>
2026-08-25 14:05:02 +0300

    gimbal range coerce

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/com/kcg/dr/flight/dji/DJIGimbal.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/com/kcg/dr/flight/dji/DJIGimbal.kt
@@ -1,12 +1,13 @@
 package com.kcg.dr.flight.dji
 
-import com.kcg.dr.flight.AircraftController.IGimbal
 import com.kcg.dr.djiutils.await
 import com.kcg.dr.djiutils.ifConnected
+import com.kcg.dr.flight.AircraftController.IGimbal
 import dji.sdk.keyvalue.key.GimbalKey
 import dji.sdk.keyvalue.value.common.Attitude
 import dji.sdk.keyvalue.value.common.EmptyMsg
 import dji.sdk.keyvalue.value.gimbal.GimbalAngleRotation
+import dji.sdk.keyvalue.value.gimbal.GimbalAttitudeRange
 import dji.sdk.keyvalue.value.gimbal.GimbalMode
 import dji.sdk.keyvalue.value.gimbal.GimbalResetType
 import dji.v5.et.action
@@ -17,6 +18,7 @@ import dji.v5.et.set
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class DJIGimbal : IGimbal {
+    private val _attitudeRange = MutableStateFlow<GimbalAttitudeRange?>(null)
     private val _attitude = MutableStateFlow(Attitude())
     override val attitude = _attitude
 
@@ -27,9 +29,13 @@ class DJIGimbal : IGimbal {
     }
 
     override suspend fun reset() = ifConnected {
-        GimbalKey.KeyGimbalAttitude.create().cancelListen(this)
-        GimbalKey.KeyGimbalAttitude.create().listen(this) {
-            it?.let { _attitude.value = it }
+        GimbalKey.KeyGimbalAttitude.create().apply {
+            cancelListen(this)
+            listen(this) { it?.let { _attitude.value = it } }
+        }
+        GimbalKey.KeyGimbalAttitudeRange.create().apply {
+            cancelListen(this)
+            listen(this) { _attitudeRange.value = it }
         }
 
         await { onSuccess: ((EmptyMsg?) -> Unit), onFailure ->
@@ -45,6 +51,13 @@ class DJIGimbal : IGimbal {
         mode: GimbalMode?
     ) {
         mode?.let { setCameraGimbalMode(it) }
+        _attitudeRange.value?.let {
+            rotation.apply {
+                pitch = pitch.coerceIn(it.pitch.min, it.pitch.max)
+                yaw = yaw.coerceIn(it.yaw.min, it.yaw.max)
+                roll = roll.coerceIn(it.roll.min, it.roll.max)
+            }
+        }
         await { onSuccess: ((EmptyMsg?) -> Unit), onFailure ->
             GimbalKey.KeyRotateByAngle.create().action(rotation, onSuccess, onFailure)
         }
